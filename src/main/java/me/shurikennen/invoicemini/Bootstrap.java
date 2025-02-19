dd401ab2a2787fa8c436c6a45db4efaaa2b2a143
Bert Garretsen <bertgarretsen2002@gmail.com>
2025-02-19 22:22:54 +0100

    Initial Commit

---
+++ src/main/java/me/shurikennen/invoicemini/Bootstrap.java
@@ -0,0 +1,16 @@
+package me.shurikennen.invoicemini;
+
+import javax.swing.*;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Test test");
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
